e2f8d37c02c5a278c5f4291af9d38a59b9738507
K-Iovenko <kyrylo.iovenko.25@kse.org.ua>
2026-07-22 13:28:02 +0300

    Calculated SMA long and short using rolling pandas command

--- main.py
+++ main.py
@@ -5,3 +5,5 @@ data = yf.download(tickers='PLTR', start="2025-06-01", end="2026-07-21")
 if isinstance(data.columns, pd.MultiIndex):
     data.columns = data.columns.get_level_values(0)
 print(data.head())
+data['SMA_Short'] = data['Close'].rolling(window = 20).mean()
+data['SMA_Long'] = data['Close'].rolling(window = 50).mean()
