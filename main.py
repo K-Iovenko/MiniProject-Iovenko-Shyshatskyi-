ea4978b14c95bc6b27d75945bca6298c74dbc821
K-Iovenko <kyrylo.iovenko.25@kse.org.ua>
2026-07-22 13:23:49 +0300

    Downloaded ticker data during certain period and cleared the column names

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import yfinance as yf
+data = yf.download(tickers='PLTR', start="2025-06-01", end="2026-07-21")
+if isinstance(data.columns, pd.MultiIndex):
+    data.columns = data.columns.get_level_values(0)
+print(data.head())
