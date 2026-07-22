2de05fa86fb1974b963cb7c5c0d0064c925c7551
K-Iovenko <kyrylo.iovenko.25@kse.org.ua>
2026-07-22 15:15:37 +0300

    sell signals buy signals distincts dataframes for identifying buy and sell points on graph

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import yfinance as yf
-data = yf.download(tickers='PLTR', start="2025-06-01", end="2026-07-21")
+data = yf.download(tickers='PLTR', start="2025-05-01", end="2026-07-21")
 if isinstance(data.columns, pd.MultiIndex):
     data.columns = data.columns.get_level_values(0)
 
 data['SMA_Short'] = data['Close'].rolling(window = 20).mean()
 data['SMA_Long'] = data['Close'].rolling(window = 50).mean()
-print(data.head(30))
+
+data['signal'] = (data['SMA_Short']>data['SMA_Long']).astype(int)
+
+buy_signals = data[data['signal'].diff() == 1]
+sell_signals = data[data['signal'].diff() == -1]
+print(data.head(50))
+print('buy signals')
+print(buy_signals.head(50))
+print('sell signals')
+print(sell_signals.head(50))
