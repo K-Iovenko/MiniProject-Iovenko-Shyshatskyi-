2fe30324fd3bede31f9ddb1d7e3471cc30229b22
K-Iovenko <kyrylo.iovenko.25@kse.org.ua>
2026-07-23 23:56:00 +0300

    Minor changes Data and rudimentary code

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import yfinance as yf
-data = yf.download(tickers='PLTR', start="2025-05-01", end="2026-07-21")
+data = yf.download(tickers='PLTR', start="2025-06-01", end="2026-07-21")
 if isinstance(data.columns, pd.MultiIndex):
     data.columns = data.columns.get_level_values(0)
 
@@ -12,11 +12,7 @@ data['signal'] = (data['SMA_Short']>data['SMA_Long']).astype(int)
 
 buy_signals = data[data['signal'].diff() == 1]
 sell_signals = data[data['signal'].diff() == -1]
-print(data.head(50))
-print('buy signals')
-print(buy_signals.head(50))
-print('sell signals')
-print(sell_signals.head(50))
+
 
 data['returns'] = data['Close'].pct_change()
 data['strategy_returns'] = data['returns'] * data['signal'].shift(1)
